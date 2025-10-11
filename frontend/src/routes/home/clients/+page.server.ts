9a7bf52709f4e85d24ded36e9119db532709d889
OleksiuDatsko <datskooleksiu@gmail.com>
2025-10-11 13:42:00 +0300

    added main clients page

--- frontend/src/routes/home/clients/+page.server.ts
+++ frontend/src/routes/home/clients/+page.server.ts
@@ -1,10 +1,24 @@
+import { fail } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ depends, locals: { supabase } }) => {
     depends('supabase:db:clients');
-    const { data: clients } = await supabase
-        .from('clients')
-        .select('*')
-        .order('first_name');
-    return { clients: clients ?? [] };
+
+    const [clientsResponse, clientTypesResponse] = await Promise.all([
+        supabase.from('clients').select('*,cars(*)'),
+        supabase.from('client_types').select('*'),
+    ]);
+
+    if (clientsResponse.error) {
+        console.error('Error fetching providers:', clientsResponse.error);
+        throw fail(500, { message: 'Failed to load providers.' });
+    }
+
+    if (clientTypesResponse.error) {
+        console.error('Error fetching warehouses:', clientTypesResponse.error);
+        throw fail(500, { message: 'Failed to load warehouses.' });
+    }
+
+
+    return { clients: clientsResponse.data ?? [], clientTypes: clientTypesResponse.data ?? [] };
 };
